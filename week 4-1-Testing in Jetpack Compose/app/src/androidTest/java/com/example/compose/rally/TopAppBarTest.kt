ed83fbab4b6466980e03fe5000b826ce800bdb99
KyungJun <kkj560@users.noreply.github.com>
2021-11-26 01:46:14 +0900

    Merged and unmerged Semantics trees
    .toUpperCase() is deprecated.
    unmerged Semantics works.

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,10 +1,8 @@
 package com.example.compose.rally
 
 
-import androidx.compose.ui.test.assertIsNotSelected
-import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
 import org.junit.Test
@@ -45,8 +43,24 @@ class TopAppBarTest {
         }
 
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
             .assertExists()
+
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Overview.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Overview.name)
+                        ),
+                useUnmergedTree = true
+            )
+            .assertDoesNotExist()
     }
 
 }
